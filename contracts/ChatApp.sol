f936d98d8898101caff6e03a255292cfb36ada11
Abid2014331053 <ibnabid12345@gmail.com>
2023-08-14 15:47:22 +0600

    Update ChatApp.sol

--- contracts/ChatApp.sol
+++ contracts/ChatApp.sol
@@ -49,12 +49,65 @@ contract ChatApp{
 
     //Add friend
 
-    function addFriend(address friend_key, string calldata name)external {
+    function addFriend(address friend_key, string calldata name) external {
         require(checkUserExist(msg.sender), "Create an account first");
         require(checkUserExist(friend_key), "User is not registered");
+        require(msg.sender != friend_key, "User Can't add themselves as friends");
+        require(checkAlreadyFriends(msg.sender, friend_key)==false, "These users are already friend");
+
+        _addFriend(msg.sender, friend_key, name);
+        _addFriend(friend_key, msg.sender, userList[msg.sender].name);
+
         
     }
 
+    //Check Already Friends
+    function checkAlreadyFriends(address pubkey1, address pubkey2) internal view returns(bool){
+        if(userList[pubkey1].friendList.length == userList[pubkey2].friendList.length){
+            address temp= pubkey1;
+            pubkey1= pubkey2;
+            pubkey2= temp;
+        }
 
+        for(uint i=0; i<userList[pubkey1].friendList.length; i++){
+            if(userList[pubkey1].friendList[i].pubkey == pubkey2) 
+                return true;
+        }
+            return false;
+    }
+
+    //Add friend
+    function _addFriend(address me, address friend_key, string memory name) internal{
+        friend memory newFriend = friend(friend_key, name);
+        userList[me].friendList.push(newFriend);
+    }
+
+
+
+    //Get My Friends
+    function getMyFriendList() external view returns(friend[] memory){
+        return userList[msg.sender].friendList;
+
+    }
+
+    //Get Chat code
+    function _getChatCode(address pubkey1, address pubkey2) internal pure returns(bytes32){
+        if(pubkey1< pubkey2){
+            return keccak256(abi.encodePacked(pubkey1, pubkey2));
+        }
+        else return keccak256(abi.encodePacked(pubkey2, pubkey1));  
+    }
+
+    function sendMessage(address friend_key, string calldata _msg) external{
+        require(checkUserExist(msg.sender), "Create an account first" );
+        require(checkUserExist(friend_key),"User is not registered");
+        require(checkAlreadyFriends(msg.sender, friend_key),"You are not friend with the given user ");
+
+        bytes32 chatcode = _getChatCode(msg.sender, friend_key );
+
+        message memory newMsg = message(msg.sender, block.timestamp, _msg);
+        allMessages
+    }
+ 
 
 }
