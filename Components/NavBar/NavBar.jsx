0ea1f2731a647bfd787bf9538b7cd3e063f4cb21
Abid2014331053 <ibnabid12345@gmail.com>
2023-08-16 17:42:39 +0600

    .

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, {useEffect, useState, useContext} from "react";
+import Image from "next/image";
+import Link from "next/link";
 
+
+
+//Internal Import
 import Style from './NavBar.module.css';
+import {ChatAppContext} from '../../Context/ChatAppContext';
+import {Model, Error} from "../index";
+import images from '../../assets';
+
+
+
 
 const NavBar = () => { 
-  return 
-  <div>NavBar</div>;
+  const menuItems = [
+    {
+      menu: "All Users",
+      link: "alluser",
+    },
+    {
+      menu: "All Users",
+      link: "alluser",
+    },
+    {
+      menu: "All Users",
+      link: "alluser",
+    },
+    {
+      menu: "All Users",
+      link: "alluser",
+    },
+    {
+      menu: "All Users",
+      link: "alluser",
+    },
+  ] 
+  return (
+    <div className={Style.NavBar}>
+      <div className={Style.NavBar_box}>
+
+      </div>
+    </div>
+  )
   
 };
 
